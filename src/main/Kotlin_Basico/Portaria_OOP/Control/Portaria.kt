347b77d10d7146697ee19e3e4c32d82025ac1493
Fred Alisson <102878055+FredAlissonx@users.noreply.github.com>
2022-11-16 11:39:23 -0300

    Add files via upload

--- src/main/Kotlin_Basico/Portaria_OOP/Control/Portaria.kt
+++ src/main/Kotlin_Basico/Portaria_OOP/Control/Portaria.kt
@@ -1,15 +1,31 @@
-package Portaria_OOP
+package Portaria_OOP.Control
+
+import Portaria_OOP.Business.ConvidadoBusiness
+import Portaria_OOP.Entity.Convidado
 
 class Portaria{
-
-    fun controle(){
+    private val convidadoBusiness = ConvidadoBusiness()
+    init{
+        println("Portaria inicializada")
+        println(controle())
+    }
+    private fun controle(): String{
         val idade = Console.readInt("Digite sua idade: ")
-        if (idade < 18){
-            println("Negado, menores de idade não são permitidos.")
-            return
+        val convidado = Convidado(idade = idade) // Valida idade com o valor recebido no input
+
+        if (!convidadoBusiness.maiorDeIdade(convidado.idade)){
+            return "Negado, menores de idade não são permitidos."
         }
 
-        val tipoConvite = Console.readString("Digite o tipo do seu convite: ")
-        if (tipoConvite )
+        convidado.tipo = Console.readString("Digite o tipo do seu convite: ")
+        if (!convidadoBusiness.tipoValido(convidado.tipo)){
+            return "Negado. Convite inválido."
+        }
+
+        convidado.codigo = Console.readString("Escreva o tipo de convite: ")
+        if (!convidadoBusiness.codigoValido(convidado)){
+            return "Negado. Codigo inválido."
+        }
+        return "Seja bem vindo :)"
     }
 }
